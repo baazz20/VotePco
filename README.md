84311b6117e47c65a87afee74228d71827f2b399
baazz20 <yaoeloge@outlook.fr>
2021-10-29 03:20:47 +0000

    setup terminer avec configuration du template et construction de la base de données, une mise en place de la section admin aussi

---
+++ README.md
@@ -0,0 +1,11 @@
+# PLATEFORME DES ELECTIONS DU PCO JI 2022
+Une plate forme de vote en ligne pour les elections du presdent du commiter d’organisation (pco) de la journee d’integration 2022 (ji2022)
+Page de connection reserve uniquement au personne autoriser a voter
+Une section administration reserve au comite d’organisation des elections
+Section destine a presenter les candidats et leur projet
+Section destine aux votes
+Section pour les resultats
+# INSTALLATION
+Clonner le depot git, placer le dossier dans le repertoire www de votre server local. <br> import la base de données avec le même nom que le fichier sql dans le dossier database. <br>
+# EN IMAGE:
+! [interface ](/public/assets/media/demo1.gif)
--- database/votePco.sql
+++ database/votePco.sql
@@ -1,59 +1,27 @@
-/*==============================================================*/
-/* Nom de SGBD :  MySQL 5.0                                     */
-/* Date de cr�ation :  28/10/2021 23:05:02                      */
-/*==============================================================*/
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : ven. 29 oct. 2021 à 01:08
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-DROP TABLE IF EXISTS CANDIDATS;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-DROP TABLE IF EXISTS JURE;
+--
+-- Base de données : `votepco`
+--
+COMMIT;
 
-DROP TABLE IF EXISTS VOTANT;
-
-/*==============================================================*/
-/* Table : CANDIDATS                                            */
-/*==============================================================*/
-CREATE TABLE CANDIDATS
-(
-   IDCANDIDAT           INT NOT NULL AUTO_INCREMENT,
-   NOM                  VARCHAR(100) NOT NULL,
-   PRENOM               VARCHAR(100) NOT NULL,
-   SEXE                 CHAR(1) NOT NULL,
-   PHOTO                VARCHAR(255) NOT NULL,
-   PRIMARY KEY (IDCANDIDAT)
-);
-
-/*==============================================================*/
-/* Table : JURE                                                 */
-/*==============================================================*/
-CREATE TABLE JURE
-(
-   IDJURE               INT NOT NULL AUTO_INCREMENT,
-   NOM                  VARCHAR(100) NOT NULL,
-   PRENOM               VARCHAR(100) NOT NULL,
-   GRADE                INT NOT NULL,
-   PRIMARY KEY (IDJURE)
-);
-
-/*==============================================================*/
-/* Table : VOTANT                                               */
-/*==============================================================*/
-CREATE TABLE VOTANT
-(
-   IDVOTANT             INT NOT NULL AUTO_INCREMENT,
-   IDJURE               INT NOT NULL,
-   IDCANDIDAT           INT NOT NULL,
-   NOM                  VARCHAR(100) NOT NULL,
-   PRENOM               VARCHAR(100) NOT NULL,
-   EMAIL                VARCHAR(100) NOT NULL,
-   SEXE                 CHAR(1) NOT NULL,
-   DATE                 DATETIME NOT NULL,
-   PRIMARY KEY (IDVOTANT)
-);
-
-ALTER TABLE VOTANT ADD CONSTRAINT FK_BANIR FOREIGN KEY (IDJURE)
-      REFERENCES JURE (IDJURE) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
-ALTER TABLE VOTANT ADD CONSTRAINT FK_VOTE FOREIGN KEY (IDCANDIDAT)
-      REFERENCES CANDIDATS (IDCANDIDAT) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
